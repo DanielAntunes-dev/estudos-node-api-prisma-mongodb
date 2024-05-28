2051a27d5ccd991d6d446b929a121ea15598af5a
Caio Henrique <dap.junioruvv@gmail.com>
2024-05-28 16:06:44 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,56 @@
+import express, { response } from "express";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const app = express();
+app.use(express.json());
+
+
+app.post("/usuarios", async (require, response) => {
+    
+  await prisma.user.create({
+    data: {
+      email: require.body.email,
+      name: require.body.name,
+      age: require.body.age,
+    },
+  });
+  response.status(201).json(require.body);
+});
+
+app.get("/usuarios",async (require, response) => {
+
+  const users = await prisma.user.findMany()
+
+  response.status(200).json(users);
+});
+
+app.put("/usuarios/:id", async (require, response) => {
+    
+  await prisma.user.update({
+    where: {
+      id: require.params.id
+    },
+    data: {
+      email: require.body.email,
+      name: require.body.name,
+      age: require.body.age,
+    },
+  });
+  response.status(201).json(require.body);
+});
+
+app.delete('/usuarios/:id', async(require, response) => {
+  await prisma.user.delete({
+    where: {
+      id: require.params.id
+    }
+  })
+  response.status(200).json({message: 'Usuário deletado com suceso!'});
+})
+
+
+app.listen(3000, () => {
+  console.log("Deu bom!!");
+});
